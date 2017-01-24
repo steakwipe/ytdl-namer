2f2fd121c66dd72dbcf51cb7fdd88efaa69b8a89
Chris Tabor <ctabor@isilon.com>
2017-01-24 08:37:06 -0800

    Some PEP-8 fixes.

--- nowplaying.py
+++ nowplaying.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # the only encoding supported by my machine is utf-8
 # going for the record of shittiest overcommented script of 2017
 # by steakwipe
 
 # Python loads DBUS so we can ask it things, and SYS so we can handle unicode (i think).
-import dbus,sys
+import dbus
+import sys
 
 # Here we're just giving our bus a nickname. bus
 bus = dbus.SessionBus()
 
-# Here, we're telling Python that when we say 'player' we mean clementine. Later we'll expand this to check for different players.
+# Here, we're telling Python that when we say 'player' we mean clementine.
+# Later we'll expand this to check for different players.
 player = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2', 'byte_arrays=True')
 
 # Finally we tell Python what we require of clementine. Song metadata.
@@ -30,5 +32,4 @@ print(metadata[u'xesam:title']).encode('utf-8'),
 print ("performed by"),
 print(metadata[u'xesam:artist'][0]).encode('utf-8'),
 print (" || "),
-#print(metadata[u'xesam:albumArtist']).encode('utf-8') # broken if no tag, commented for now
-
+# print(metadata[u'xesam:albumArtist']).encode('utf-8') # broken if no tag, commented for now
--- yt-tagger.py
+++ yt-tagger.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
-#YouTube URL generator/tagger
-#by steakwipe with a ridiculous amount of help from beatsteak
-# ##steakhouse // #ninjapirate // freenode
+# -*- coding: utf-8 -*-
 
-#commenty var reminders for now
+"""
+YouTube URL generator/tagger
+by steakwipe with a ridiculous amount of help from beatsteak
+#steakhouse // #ninjapirate // freenode
+"""
 
-#yt="https://youtu.be/"
-#name=specific filename
-#URL=garbly part
-#f=music directory. make this modifiable at runtime later!!!
+# commenty var reminders for now
+
+# yt="https://youtu.be/"
+# name=specific filename
+# URL=garbly part
+# f=music directory. make this modifiable at runtime later!!!
 
 import os
 os.listdir(f)
