ea0eaac1b1f069641803801f65b8a2568f510637
Ray Miller <steakwipe@gmail.com>
2017-01-24 03:46:39 -0800

    Spent entirely too long yelling at unicode

--- nowplaying.py
+++ nowplaying.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python
-# encoding: utf-8
+#-*- coding: utf-8 -*-
 # the only encoding supported by my machine is utf-8
 # going for the record of shittiest overcommented script of 2017
 # by steakwipe
 
-# Python loads DBUS so we can ask it things.
+# Python loads DBUS so we can ask it things, and SYS so we can handle unicode (i think).
 import dbus,sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-# Here we're just giving our bus a nickname. "bus"
+# Here we're just giving our bus a nickname. bus
 bus = dbus.SessionBus()
-# Here, we're telling Python that when we say "player" we mean clementine.
-player = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2')
+
+# Here, we're telling Python that when we say 'player' we mean clementine. Later we'll expand this to check for different players.
+player = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2', 'byte_arrays=True')
 
 # Finally we tell Python what we require of clementine. Song metadata.
-metadata = player.Get('org.mpris.MediaPlayer2.Player', 'Metadata', dbus_interface='org.freedesktop.DBus.Properties')
+metadata = player.Get( 'org.mpris.MediaPlayer2.Player', 'Metadata', dbus_interface='org.freedesktop.DBus.Properties')
 
-# This is just the unnatural fuckery I used to cut the Artist and Title
-# out of the metadata. The commas at the end of the line tell Python
-# not to start a new line when running. This causes the entire output to go along one line, ready for spamming into IRC
-print("Now playing: "),
+#i was doing it wrong
+#print('Now playing:',
+#	(metadata[u'xesam:title']),
+#	 'performed by',
+##	 'from',
+#	 (metadata[u'xesam:album']),
+#).encode('utf-8')
+
+# This shit works below
+print("Now playing:"),
 print(metadata[u'xesam:title']).encode('utf-8'),
 print ("performed by"),
-print(metadata[u'xesam:albumArtist'][0]).encode('utf-8')
-
-
-
-####
-# todo, cause fuck it why not #
-			   ####
-# should probably eventually wrap this up for hexchat consumption
-# current scripts available online do not seem to work because
-# clementine does not appear to support MPRIS1.0 any longer, unable to verify via irc though.
-# wtf
-# reach out to folks who are familiar with hexchat scripts to find out
-# the bare minimum i'd need to execute from within hexchat. 
-# 
-# attempt hexchat.command("me switchover- requires actually being loaded into hexchat i'm sure.
+print(metadata[u'xesam:artist'][0]).encode('utf-8'),
+print (" || "),
+#print(metadata[u'xesam:albumArtist']).encode('utf-8') # broken if no tag, commented for now
 
