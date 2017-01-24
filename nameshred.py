29d314bcadc060cbaa116954e5e2a1fe205d409a
Ray Miller <steakwipe@gmail.com>
2017-01-24 07:01:51 -0800

    Syncing up before I dig into the name smasher

---
+++ nameshred.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*- #
+#nameshredder script
+
+import os,sys
+
---
+++ yt-tagger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#YouTube URL generator/tagger
+#by steakwipe with a ridiculous amount of help from beatsteak
+# ##steakhouse // #ninjapirate // freenode
+
+#commenty var reminders for now
+
+#yt="https://youtu.be/"
+#name=specific filename
+#URL=garbly part
+#f=music directory. make this modifiable at runtime later!!!
+
+import os
+os.listdir(f)
+
+
+os.path.basename(filelist.txt)
